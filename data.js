6573179d6dad04262280fc9bd27adf616ed07999
RazorSh4rk <iamrazorshark@gmail.com>
2020-04-06 14:47:34 +0200

    disable stuff

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 const dungeons = [
     {
-        name: 'FASZ dungeon',
+        name: 'dungeon 1',
         image: 'assets/asd.png',
         tier: 1,
         counter: 0,
         times: [2, 4224, 321]
     },
     {
-        name: 'SEGG dungeon',
+        name: 'dungeon 2',
         image: 'assets/asd.png',
         tier: 2,
         counter: 0,
